089049bdf3394cdb0bffb5a449d6899f2cbf5160
Vaibhav Srivastav <vaibhavs10@gmail.com>
2023-03-08 16:12:22 +0000

    update move_files

--- move_files.py
+++ move_files.py
@@ -6,12 +6,12 @@ from tqdm import tqdm
 # To change according to version and language
 # ---------
 lang = "ab"
-clip_path = f"/home/polina_huggingface_co/data/cv11/cv-corpus-11.0-2022-09-21/{lang}/clips"
+clip_path = f"/home/vaibhav_huggingface_co/common_voice_dataset_generator/data/{lang}/cv-corpus-12.0-2022-12-07/{lang}/clips"
 # ---------
 
 splits = ("test", "dev", "train", "other", "invalidated", "validated")
 for split in splits:
-    data = pandas.read_csv(f"/home/polina_huggingface_co/data/cv11/cv-corpus-11.0-2022-09-21/{lang}/{split}.tsv", sep='\t')
+    data = pandas.read_csv(f"/home/vaibhav_huggingface_co/common_voice_dataset_generator/data/{lang}/cv-corpus-12.0-2022-12-07/{lang}/{split}.tsv", sep='\t')
     all_files = [os.path.join(clip_path, f) for f in list(data["path"])]
 
     # nums = [f.split("_")[-1].split(".mp3")[0] for f in all_files]
@@ -42,37 +42,3 @@ for split in splits:
         command = f"tar -cvf {new_clip_path}/{directory}.tar {new_clip_path}/{directory}"
         print(command)
         os.system(command)
-
-    # dirs = {}
-    # dir_idx = list(set(sorted([num // num_per_dir for num in nums])))
-    # dir_idx.append(max_num // num_per_dir + 1)
-    # for i in dir_idx:
-    #     start_idx = i * num_per_dir
-    #     end_index = min((i + 1) * num_per_dir, max_num)
-    #     path = dir_path.format(start_idx=start_idx, end_index=end_index)
-    #     target_dir = os.path.join(new_clip_path, path)
-    #     dirs[path] = target_dir
-    #     command = f"mkdir -p {target_dir}"
-    #     print(command)
-    #     os.system(command)
-    #
-
-    # for file in tqdm.tqdm(all_files):
-    #     num = int(file.split("_")[-1].split(".mp3")[0])
-    #     start_idx = (num // num_per_dir) * num_per_dir
-    #     end_index = min(start_idx + num_per_dir, max_num)
-    #
-    #     path = dir_path.format(start_idx=start_idx, end_index=end_index)
-    #
-    #     target_dir = dirs[path]
-    #     file = file.split("/")[-1]
-    #
-    #     command = f"mv {os.path.join(clip_path, file)} {os.path.join(target_dir, file)}"
-    #     os.system(command)
-    #
-    # all_dirs = [d for d in os.listdir(new_clip_path) if os.path.isdir(os.path.join(new_clip_path, d))]
-    # for directory in all_dirs:
-    #     command = f"tar -zcvf {new_clip_path}/{directory}.tar.gz {new_clip_path}/{directory}"
-    #     print(command)
-    #     os.system(command)
-
